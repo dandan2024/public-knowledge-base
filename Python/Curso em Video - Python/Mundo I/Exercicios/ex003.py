3612c86867ec9d8426d3da2d7bcdedd8ef8f0387
dandan2024 <dan_santosrv@hotmail.com>
2024-01-27 22:10:45 -0300

    meu primeiro commit

---
+++ Python/Curso em Video - Python/Mundo I/Exercicios/ex003.py
@@ -0,0 +1,6 @@
+#adicionar o enunciado do exercicio aqui
+
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro:'))
+s = n1+n2
+print('A soma entre {} e {} vale {}'.format(n1,n2,s))
---
+++ Python/Curso em Video - Python/Mundo I/Exercicios/ex004.py
@@ -0,0 +1,4 @@
+#adicionar o enunciado do exercicio aqui
+
+n1=input('Digite seu nome:')
+print(n1.islower())
